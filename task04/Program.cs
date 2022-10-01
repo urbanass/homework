92e15a377209e204a5193b3a1e6881d52947e0f8
UrbanAss134 <urbanass@bk.ru>
2022-10-01 23:01:17 +0500

    Создание 4 задачи2

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("start");
+int a = 2;
+int b = 3;
+int c = 7;
+//int Max(int a, int b) => a > b ? a : b;
+//int Max(int a, int b, int c) => Max(Max(a, b), c);
+//max=a;
+//if (b>max)max=b;
+//if (c>max)max=c;
+if(a > b)
+  Console.WriteLine("max=a");
+  if(max < c);
+  Console.WriteLine("max=c");
+else
+  Console.WriteLine("Max=a");
+Console.WriteLine("and");

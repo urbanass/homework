cce4668bf294de465384efebcb91c63801a94ad4
UrbanAss134 <urbanass@bk.ru>
2022-10-02 20:59:12 +0500

    задача решена

--- task08/Program.cs
+++ task08/Program.cs
@@ -1,3 +1,15 @@
-﻿Console.WriteLine("start");
-int num1=1;
-int num2 =10;
+﻿//Console.WriteLine("start");
+void Metod1(int number)
+{
+int num = 1;
+while (num <= number)
+    {
+        if (num % 2 ==0)
+        {
+         Console.WriteLine(num);
+        }
+        num++;
+    }
+}
+int number = int.Parse(Console.ReadLine());
+Metod1(number);

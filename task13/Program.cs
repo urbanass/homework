6482eec8d8dc2f1b2557f73dfedb189dc815a229
UrbanAss134 <urbanass@bk.ru>
2022-10-08 15:21:07 +0500

    задача 13 выходной

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿void Main(string[] args)
+{
+    int n = 12345;
+    if (n > 100)
+    {
+        int d = GetSecondDigit(n);
+        Console.WriteLine(d);
+    }
+    else Console.WriteLine("нет третей цифры");
+    //Console.ReadKey();
+}
+    int GetSecondDigit(int k)
+{
+    while (k >= 1000)
+    {
+        k /= 10;
+    }
+    int d = k % 10;
+    return d;
+}
+Main(args);

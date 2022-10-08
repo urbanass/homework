312c906da18a7dfc4a46601dd8f85e84b5f5d94d
UrbanAss134 <urbanass@bk.ru>
2022-10-08 14:08:32 +0500

    задача 15 выходной

---
+++ task15/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите метод, который принимает на вход цифру, 
+обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+    void Main(int day)
+{
+    if (day >= 1 && day <= 7)
+        {
+        if (day == 6 | day == 7) Console.WriteLine("Сегодня выходной");
+        else Console.WriteLine("Сегодня рабочий день");
+        }
+    else Console.WriteLine("Упс,прочитай условие");
+}
+
+Console.Write("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+Main(day);

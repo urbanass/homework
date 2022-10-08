9cd11a8c88d615f69864cf50172d95d3b4098566
UrbanAss134 <urbanass@bk.ru>
2022-10-08 16:01:22 +0500

    задача 10

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿void Main(string[] args)
+{
+    int n = 595;
+    int d = GetSecondDigit(n);
+    Console.WriteLine(d);
+    //Console.ReadKey();
+}
+
+    int GetSecondDigit(int k)
+{
+    k /= 10;
+    int d = k % 10;
+    return d;
+}
+Main(args);
